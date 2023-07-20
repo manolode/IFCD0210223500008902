37c21d40ed7e385402e6440872b763fd4599600e
manolode <manuelnaranjogutierrez@gmail.com>
2023-07-20 16:53:08 +0100

    JJJJ

--- CLASES/200723/actividades.js
+++ CLASES/200723/actividades.js
@@ -139,18 +139,17 @@
     // if (administrador == "Admin" || administrador =="admin" || administrador== "ADMIN")
     // {
     
-    // var contraseña = prompt ("contraseña:", []);
-    // if (contraseña == "webmaster"){
-    // alert ("Bienvenido");}
-    // else if (contraseña==="" || contraseña===null){
-    // alert ("cancelado");
-    // }
-    // else {
-    //     alert ("contraseña incorrecta");
-    // }
+    //     var contraseña = prompt ("contraseña:", []);
+    //     if (contraseña == "webmaster"){
+    //         alert ("Bienvenido");}
+    //          else if (contraseña==="" || contraseña===null){
+    //             alert ("cancelado");
+    //             }else {
+    //                 alert ("contraseña incorrecta");
+    //             }
 
     // }else if (administrador === "" || administrador === null){
-    // alert ("cancelado");
+    //     alert ("cancelado");
     // }else {
-    // alert ("usuario incorrecto");
+    //     alert ("usuario incorrecto");
     // }
